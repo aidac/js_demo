b8f072e62048154d5de77d48c9db9a9d1429f22b
Aidas Staškevičius <aidas@vinted.com>
2018-02-07 21:03:38 +0200

    if statement

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 "use strict";
+let a;
+let b;
 
-let a = +prompt("iveskite skaiciu a");
-let b = +prompt("iveskite skaiciu b");
+a = +prompt("iveskite skaiciu a");
+if (isNaN(a)) {
+    do {
+        a = +prompt("iveskite SKAICIU a");
+    } while (isNaN(a));
+}
+
+b = +prompt("iveskite skaiciu b");
+if (isNaN(b)) {
+    do {
+        b = +prompt("iveskite SKAICIU b");
+    } while (isNaN(b));
+}
 
 document.write(`a = ${a}, b = ${b}<br>a + b = ${a + b}<br> a - b = ${a - b}<br> a | b = ${a % b}`);
