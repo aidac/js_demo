ca1111d3e3819281b73387ecb7b4df556295b88a
Aidas Staškevičius <aidas@vinted.com>
2018-02-07 20:31:51 +0200

    short
    short

--- js/script.js
+++ js/script.js
@@ -1,15 +1,6 @@
 "use strict";
 
-let a;
-let a_str;
-let b;
-let b_str;
-
-
-a_str = prompt("iveskite skaiciu a");
-b_str = prompt("iveskite skaiciu b");
-
-a = Number(a_str);
-b = Number(b_str);
+let a = +prompt("iveskite skaiciu a");
+let b = +prompt("iveskite skaiciu b");
 
 document.write(`a = ${a}, b = ${b}<br>a + b = ${a + b}<br> a - b = ${a - b}<br> a | b = ${a % b}`);
